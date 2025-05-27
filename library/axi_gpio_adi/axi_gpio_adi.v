e5765247bd4a8c4f60a2582156c34eec262b233f
mandrica <maria-zoita.andrica@analog.com>
2025-05-27 14:54:30 +0300

    Change name at files and create GPIO IP core

---
+++ library/axi_gpio_adi/axi_gpio_adi.v
@@ -0,0 +1,145 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright (C) 2019-2023 Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/main/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+`timescale 1ns / 1ps
+
+module axi_gpio_adi #(
+  //axi interface
+  input                   s_axi_aclk,
+  input                   s_axi_aresetn,
+  input                   s_axi_awvalid,
+  input       [15:0]      s_axi_awaddr,
+  input       [ 2:0]      s_axi_awprot,
+  output                  s_axi_awready,
+  input                   s_axi_wvalid,
+  input       [31:0]      s_axi_wdata,
+  input       [ 3:0]      s_axi_wstrb,
+  output                  s_axi_wready,
+  output                  s_axi_bvalid,
+  output      [ 1:0]      s_axi_bresp,
+  input                   s_axi_bready,
+  input                   s_axi_arvalid,
+  input       [15:0]      s_axi_araddr,
+  input       [ 2:0]      s_axi_arprot,
+  output                  s_axi_arready,
+  output                  s_axi_rvalid,
+  output      [ 1:0]      s_axi_rresp,
+  output      [31:0]      s_axi_rdata,
+  input                   s_axi_rready,
+  
+  output reg  [3:0]   	  gpio_leds,
+  input  wire [3:0]       gpio_buttons
+);
+  reg           up_resetn = 1'b0;
+  wire          up_clk;
+  assign up_clk = s_axi_aclk;
+
+  up_axi #(
+    .AXI_ADDRESS_WIDTH(10)
+  ) i_up_axi (
+    .up_rstn (s_axi_aresetn),
+    .up_clk (up_clk),
+    .up_axi_awvalid (s_axi_awvalid),
+    .up_axi_awaddr (s_axi_awaddr),
+    .up_axi_awready (s_axi_awready),
+    .up_axi_wvalid (s_axi_wvalid),
+    .up_axi_wdata (s_axi_wdata),
+    .up_axi_wstrb (s_axi_wstrb),
+    .up_axi_wready (s_axi_wready),
+    .up_axi_bvalid (s_axi_bvalid),
+    .up_axi_bresp (s_axi_bresp),
+    .up_axi_bready (s_axi_bready),
+    .up_axi_arvalid (s_axi_arvalid),
+    .up_axi_araddr (s_axi_araddr),
+    .up_axi_arready (s_axi_arready),
+    .up_axi_rvalid (s_axi_rvalid),
+    .up_axi_rresp (s_axi_rresp),
+    .up_axi_rdata (s_axi_rdata),
+    .up_axi_rready (s_axi_rready),
+    .up_wreq (up_wreq_s),
+    .up_waddr (up_waddr_s),
+    .up_wdata (up_wdata_s),
+    .up_wack (up_wack),
+    .up_rreq (up_rreq_s),
+    .up_raddr (up_raddr_s),
+    .up_rdata (up_rdata),
+    .up_rack (up_rack));
+
+  reg [3:0] gpio_leds_reg;
+//axi registers write
+  assign gpio_leds = gpio_leds_reg;
+  always @(posedge up_clk) begin
+	if (up_resetn == 1'b1) begin
+		if ((up_wreq_s == 1'b1) && (up_waddr_s == 8'h02)) begin
+			gpio_leds <= up_wdata_s[3:0]; 
+		end
+		if ((up_wreq_s == 1'b1) && (up_waddr_s == 8'h21)) begin
+			gpio_buttons <= up_wdata_s[3:0];  
+		end
+	
+ //writing reset
+  always @(posedge up_clk) begin
+    if (s_axi_aresetn == 1'b0) begin
+      up_wack <= 'd0;
+      up_resetn <= 1'd0;
+    end else begin
+      up_wack <= up_wreq_s;
+      if ((up_wreq_s == 1'b1) && (up_waddr_s == 8'h20)) begin
+        up_resetn <= up_wdata_s[0];
+      end else begin
+        up_resetn <= 1'd1;
+      end
+    end
+  end
+
+  //axi registers read
+  always @(posedge up_clk) begin
+    if (s_axi_aresetn == 1'b0) begin
+      up_rack <= 'd0;
+      up_rdata <= 'd0;
+    end else begin
+      up_rack <= up_rreq_s;
+      if (up_rreq_s == 1'b1) begin
+        case (up_raddr_s)
+          8'h00: up_rdata <= gpio_leds_reg;
+          8'h04: up_rdata <= gpio_buttons;
+          default: up_rdata <= 0;
+        endcase
+      end else begin
+        up_rdata <= 32'd0;
+      end
+    end
+  end
+
+endmodule
--- library/axi_gpio_adi/axi_gpio_adi_ip.tcl
+++ library/axi_gpio_adi/axi_gpio_adi_ip.tcl
@@ -7,13 +7,12 @@
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
-adi_ip_create axi_fan_control
-adi_ip_files axi_fan_control [list \
+adi_ip_create axi_gpio_adi
+adi_ip_files axi_gpio_adi [list \
   "$ad_hdl_dir/library/common/up_axi.v" \
-  "$ad_hdl_dir/library/common/util_pulse_gen.v" \
-  "axi_fan_control.v"]
+  "axi_gpio_adi.v"]
 
-adi_ip_properties axi_fan_control
+adi_ip_properties axi_gpio_adi
 
 set_property company_url {https://wiki.analog.com/resources/fpga/docs/axi_fan_control} [ipx::current_core]
 
